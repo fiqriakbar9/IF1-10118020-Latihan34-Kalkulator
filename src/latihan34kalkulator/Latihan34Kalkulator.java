7d293db0aaaacdfb5f53bba2ffc9597392b0b338
fiqriakbar9 <42399322+fiqriakbar9@users.noreply.github.com>
2019-10-15 19:13:07 +0700

    Add files via upload

---
+++ src/latihan34kalkulator/Latihan34Kalkulator.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan34kalkulator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 'KoalaX'
+ * NAMA     : Fiqri Akbar Pratama
+ * KELAS    : PBO1/IF1
+ * NIM      : 10118020
+ * Deksripsi Program : Program ini berisi program kalkulator sederhana yang 
+ * berbasis objek oriented
+ */
+public class Latihan34Kalkulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        Kalkulator hitung = new Kalkulator();
+        
+        System.out.println("====Aplikasi Kalkulator Bilangan====");
+        System.out.printf("Masukkan Angka ke-1 : ");
+        hitung.value1 = scanner.nextDouble();
+        System.out.printf("Masukkan Angka ke-2 : ");
+        hitung.value2 = scanner.nextDouble();
+        System.out.println("");
+        
+        System.out.println("Hasil Pertambahan : " + hitung.tambahBilangan());
+        System.out.println("Hasil Pengurangan : " + hitung.kurangBilangan());
+        System.out.println("Hasil Perkalian : " + hitung.kaliBilangan());
+        System.out.println("Hasil Pembagian : " + hitung.bagiBilangan());
+        System.out.println("Hasil Sisa : " + hitung.sisaBilangan());
+        System.out.println("\nDevelop by : Fiqri Akbar Pratama");
+    }
+    
+}
